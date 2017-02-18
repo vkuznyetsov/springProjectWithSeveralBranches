78f70ef6cca3f527c03d2d590b6ef2d5e9acd39e
idexmai <iedexmain@gmail.com>
2017-02-18 19:53:24 +0000

    commitchk_remoteSat Feb 18 19:53:24 UTC 2017

--- src/main/java/helloworld/GreetingController.java
+++ src/main/java/helloworld/GreetingController.java
@@ -1,5 +1,5 @@
 package helloworld;
-//1487431658688
+//1487447438185
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,4 +1,4 @@
-<!1487431725259>
+<!1487447519093>
 <%
    response.sendRedirect("spring/hello");
 %>
